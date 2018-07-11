5ea383876df50d403fc040e28891baf567b747f6
David Patrick Donohue <donohue76@hotmail.com>
2018-07-11 15:12:41 -0700

    completed more exercise

--- ex33_While_Loops.rb
+++ ex33_While_Loops.rb
@@ -15,6 +15,8 @@ puts "The numbers: "
 # remember you can write this 2 other ways: 
 numbers.each {|num| puts num }
 
+
+# Convert the above while loop to a function that can be called, and replace 6 in the test (i < 6) with a variable.
 def WhileLoops(x)
   i = 0
   numbers = []
---
+++ ex34_Accessing_Elements_of_Arrays.rb
@@ -0,0 +1,10 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# 1. The animal at 1. The animal at 1 is the second animal and is a ruby. The second animal is at 1 and is a ruby
+# 2. The third (3rd) animal. The third animal is at 2 and is a peacock. The animal at 2 is the third animal and is a peacock.
+# 3. The first (1st) animal. The first animal is at 0 and is a bear. The animal at 0 is the first aniaml and is a bear.
+# 4. The animal at 3. The animal at 3 is the 4th animal and is a kangaroo. The 4th animal is at 3 and is a kangaroo.
+# 5. The fifth (5th) animal. The fifth animal is at 4 and is a while, The animal at 4 is the 5th animal and is a whale.
+# 6. The animal at 2. The animal at 2 is the 3rd animal and is a peacock. The 3rd animal is at 2 and is a peacock.
+# 7. The sixth (6th) animal. The sixth animal is at 5 and is a platypus. The animal at 5 is the 6th animal and is a platypus.
+# 8. The animal at 4. The animal at 4 is the is the fifth animal and is a whale. The fifth animal is at 4 and is a whale.
