1e12b2440233fd0019b387aa55af45c96ff0dde5
David Patrick Donohue <donohue76@hotmail.com>
2019-01-27 12:48:32 -0800

    corrected type-o in ex34

--- ex34_Accessing_Elements_of_Arrays.rb
+++ ex34_Accessing_Elements_of_Arrays.rb
@@ -7,4 +7,4 @@ animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
 # 5. The fifth (5th) animal. The fifth animal is at 4 and is a while, The animal at 4 is the 5th animal and is a whale.
 # 6. The animal at 2. The animal at 2 is the 3rd animal and is a peacock. The 3rd animal is at 2 and is a peacock.
 # 7. The sixth (6th) animal. The sixth animal is at 5 and is a platypus. The animal at 5 is the 6th animal and is a platypus.
-# 8. The animal at 4. The animal at 4 is the is the fifth animal and is a whale. The fifth animal is at 4 and is a whale.
+# 8. The animal at 4. The animal at 4 is the fifth animal and is a whale. The fifth animal is at 4 and is a whale.
